4f72a0e0d437323bf1a2abea0867113ff12b23a2
Ferenc Péli <ferenc.peli@axzoom.de>
2022-12-04 17:02:23 +0100

    Adding frontend solution with vite, configuring different docker-compose environments

--- src/express-api/src/index.js
+++ src/express-api/src/index.js
@@ -11,6 +11,16 @@ app.get("/", (req, res) => {
 	const data = [
 		{ name: "James", age: 22 },
 		{ name: "John", age: 25 },
+		{ name: "Frank", age: 30 },
+		{ name: "Peter", age: 33 },
+	];
+	res.status(200).type("application/json").send(data);
+});
+
+app.get("/deep/url", (req, res) => {
+	const data = [
+		{ name: "James", age: 22 },
+		{ name: "John", age: 26 }
 	];
 	res.status(200).type("application/json").send(data);
 });
---
+++ src/frontend/src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { ref } from "vue";
+
+const values = ref();
+
+fetch("api/express/deep/url")
+	.then((response) => response.json())
+	.then((data) => {
+		values.value = data;
+	});
+</script>
+
+<template>
+	<div>
+		<a href="https://vitejs.dev" target="_blank">
+			<img src="/vite.svg" class="logo" alt="Vite logo" />
+		</a>
+		<a href="https://vuejs.org/" target="_blank">
+			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+		</a>
+	</div>
+	<pre>
+   {{ values }}
+  	</pre
+	>
+</template>
+
+<style scoped>
+.logo {
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
+}
+.logo:hover {
+	filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+	filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
